f90b9cc7343960cbc828557c9c677f9c006932f2
hanushkumaraan <dhanushkumarana@drgpit.ac.in>
2023-07-25 14:39:23 +0530

    main

--- sample2.js
+++ sample2.js
@@ -5,7 +5,14 @@ let name = "DK";
 let age = 23;
 let completedcollege = false;
 let tech = ["app","website","backend"]
+let person = {
+    name : "john",
+    age:25,
+};
+let salary = null;
 console.log(name);
 console.log(age);
 console.log(completedcollege);
 console.log(tech);
+console.log(typeof tech);
+console.log(typeof completedcollege);
