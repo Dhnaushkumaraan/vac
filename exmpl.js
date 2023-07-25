890342fb281939080e6a69510b0fc6dfcd5df92a
hanushkumaraan <dhanushkumarana@drgpit.ac.in>
2023-07-25 14:44:37 +0530

    all

---
+++ exmpl.js
@@ -0,0 +1,25 @@
+console.log("welcome to my webpage");
+//this one
+
+let name = "DK";
+let age = 23;
+let completedcollege = false;
+let tech = ["app","website","backend"]
+let person = {
+    name : "john",
+    age:25,
+};
+let salary = null;
+console.log(name);
+console.log(age);
+console.log(completedcollege);
+console.log(tech);
+console.log(typeof tech);
+console.log(typeof completedcollege);
+console.log(`my name is ${name}`);
+alert("kakashi")
+let value=prompt("Type somthing");
+let value1=parseInt(prompt("enter num 1"));
+let value2=parseInt(prompt("enter num 2"));
+let add=value1+value2;
+console.log(add);
