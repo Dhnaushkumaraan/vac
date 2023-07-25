8b842e85eae385eaced7c0ed0ae4f239b8c32db7
hanushkumaraan <dhanushkumarana@drgpit.ac.in>
2023-07-25 13:48:45 +0530

    dhanush

---
+++ sample2.js
@@ -0,0 +1,11 @@
+console.log("welcome to my webpage");
+//this one
+
+let name = "DK";
+let age = 23;
+let completedcollege = false;
+let tech = ["app","website","backend"]
+console.log(name);
+console.log(age);
+console.log(completedcollege);
+console.log(tech);
